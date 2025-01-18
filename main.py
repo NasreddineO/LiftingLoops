7fc1cf1078deda15b288b65b4bd73358d1539d40
Jeppe Mul <jeppe@mul.email>
2025-01-18 15:02:11 +0100

    Accidentally screwed up the visualization in previous commit, fixed it.

--- main.py
+++ main.py
@@ -65,3 +65,4 @@ if __name__ == '__main__':
 
      # --------------------------- Beam (with lookahead) ---------------------------------------
     Beam(sequence, iterations, output_file, threeD).run_experiment()
+    plt.show()
